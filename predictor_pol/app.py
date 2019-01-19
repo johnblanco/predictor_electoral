d938b3167543db57fa0604ef0aef23427920a4ce
johnblanco <j.pabloblanco@gmail.com>
2019-01-19 20:11:20 -0300

    Update app.py

--- predictor_pol/app.py
+++ predictor_pol/app.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('main.html')
 
-@app.route('/respuestas',methods=['POST'])
+@app.route('/answers',methods=['POST'])
 def answers():
+    education = request.form['education']
+    inflation = request.form['inflation']
+
+    print(education)
+    print(inflation)
+
+    #aca llamamos al modelo de machine learning con las respuestas y calculamos el candidato
+
+
     return render_template('main.html')
+
+
