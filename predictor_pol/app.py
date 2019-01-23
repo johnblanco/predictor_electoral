6e6405a039ba584dbe824666e41cd53cef763b09
johnblanco <j.pabloblanco@gmail.com>
2019-01-22 22:31:01 -0300

    envia variables al template

--- predictor_pol/app.py
+++ predictor_pol/app.py
@@ -1,23 +1,24 @@
+#para correr: export FLASK_APP=app.py;flask run
+
 from flask import Flask
 from flask import render_template
 from flask import request
+import joblib
+import pandas as pd
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def main():
-    return render_template('main.html')
+    if request.method == 'POST':
+        print(request.form)
 
-@app.route('/answers',methods=['POST'])
-def answers():
-    education = request.form['education']
-    inflation = request.form['inflation']
+    preguntas = [{'id': 1, 'texto': 'pregunta 1'},{'id': 2, 'texto': 'pregunta 2'}]
+    candidatos = [{'id': 1, 'nombre': 'candidato 1'},{'id': 2, 'nombre': 'candidato 2'}]
 
-    print(education)
-    print(inflation)
+    return render_template('main.html', preguntas=preguntas, candidatos=candidatos)
 
-    #aca llamamos al modelo de machine learning con las respuestas y calculamos el candidato
-
-
-    return render_template('main.html')
-
-
+def predict(responses):
+    xgb = joblib.load('xg_model')
+    df_test = pd.DataFrame.from_dict({'resp1': [1], 'resp2': [1]})
+    print(xgb.predict(df_test))
