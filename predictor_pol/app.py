35dccbd44bb885d4d61de59c89e32b2b9c2cd117
ElectrWeakHyprCharge <{ID}+{username}@users.noreply.github.com>
2019-01-23 21:36:05 -0300

    Seriedades
    Validar que el usuario responda todas las preguntas

--- predictor_pol/app.py
+++ predictor_pol/app.py
@@ -1,7 +1,6 @@
 #para correr: export FLASK_APP=app.py;flask run
 
 from flask import Flask, render_template, request
-import json
 import joblib
 import pandas as pd
 
@@ -10,14 +9,28 @@ app = Flask(__name__)
 
 preguntas = ['Pregunta 1', 'Pregunta 2']
 candidatos = ['Candidato 1', 'Candidato 2', 'Juan Sartori (aka u/nano2412)']
+valid_keys = {
+    'candidato',
+    *('pregunta_' + str(i) for i in range(1, len(preguntas)))
+}
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def main():
     if request.method == 'POST':
-        print(request.form)
+        if validate(request.form):
+            print(request.form)
+            # Esto habría que reemplazar por un render_template
+            return 'Gracias por contestar!'
+        else:
+            # Esto también
+            return 'Error'
 
     return render_template('main.html', preguntas=preguntas, candidatos=candidatos)
+
+
+def validate(form):
+    return all(form.get(key, '').isdecimal() for key in valid_keys)
 
 
 def predict(responses):
