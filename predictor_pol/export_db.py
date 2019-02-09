d91fa8264804fe273713415af289edb510eaf6a3
johnblanco <j.pabloblanco@gmail.com>
2019-02-09 18:18:47 -0300

    cambios en ipython

---
+++ predictor_pol/export_db.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+db = sqlite3.connect('predictor_prod.db', isolation_level=None)
+sql = '''
+select id,candidato_elegido,fecha
+from encuestas;
+'''
+
+cur = db.cursor()
+rows = cur.execute(sql).fetchall()
+
+with open('encuestas.csv','w') as f:
+    f.write('id,candidato,fecha\n')
+    for r in rows:
+        f.write('{},{},{}\n'.format(r[0],r[1],r[2]))
+
+
+sql = '''
+select id_encuesta,id_pregunta,respuesta
+from respuestas_encuestas
+where id_pregunta<6;
+'''
+
+cur = db.cursor()
+rows = cur.execute(sql).fetchall()
+
+with open('respuestas_encuestas.csv','w') as f:
+    f.write('id_encuesta,id_pregunta,respuesta\n')
+    for r in rows:
+        f.write('{},{},{}\n'.format(r[0],r[1],r[2]))
