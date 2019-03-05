0204cb597748c064c4927ff1d04c26100a77bf34
johnblanco <j.pabloblanco@gmail.com>
2019-03-05 12:14:50 -0300

    export new model

---
+++ scripts/export_models2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv("../csvs/data.csv")
+df_train, df_test = train_test_split(df, test_size=0.20)
+features = ['1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19','2', '20', '21', '22', '23', '24', '25', '26', '3', '4', '5', '6', '7','8', '9']
+clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial',max_iter=700).fit(df_train[features], df_train.candidatoId)
+joblib.dump(clf, "../predictor_pol/candidate_model2.joblib")
