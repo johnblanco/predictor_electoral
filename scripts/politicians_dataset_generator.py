8a2134fd6e3a83722cc0687df83ba9cf5ce144c8
Rodrigo Conde <conderodrigo98@gmail.com>
2019-04-23 20:54:30 -0300

    fix

--- scripts/politicians_dataset_generator.py
+++ scripts/politicians_dataset_generator.py
@@ -20,21 +20,3 @@ with open("../predictor_pol/json_data/candidatos.json") as f:
 
 with open("../csvs/random_politicians_dataset.csv", "w+", encoding='utf-8') as outf:
 	outf.write(dataset_string)
-
-#q_count = 20
-
-#with open("../predictor_pol/json_data/candidatos.json") as f:
- #   CANDIDATOS = []
-  #  file_parties = json.load(f)
-   # for party in file_parties:
-    #	CANDIDATOS.append({"party": party["party"].title(), "candidates": []})
-    #	for candidate in party["candidates"]:
-     #   	rand_answers = [];
-      #  	for i in range(20):
-       # 		rand_answers.append(random.randint(1,7))
-        #	row = {"id" : candidate["id"], "name" : candidate["name"], "answers" : rand_answers}
-        #	CANDIDATOS[-1]["candidates"].append(row)
-
-#with open("../predictor_pol/json_data/random_politicians_dataset.json", "w") as outf:
-#	json.dump(CANDIDATOS, outf, indent=4)
-#}
